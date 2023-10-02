0ee102394011ec9a4d3976e326e5f2ed89418eb2
denisa-mat <96780077+denisa-mat@users.noreply.github.com>
2023-10-02 18:12:25 +0200

    add first HW to ISKM55

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,7 @@
 // Note: Here we are getting data from a js file, but in a different project I'll be fetching these projects from some srt of APIs.
 
 const projects = [
+	// ISKM01 Informační vědy
 	{
 		id: 1,
 		type: "Kisk",
@@ -83,9 +84,9 @@ const projects = [
 				]
 			}
 		],
-
-
 	},
+
+	// ISKM02 Design informačních služeb, rozhraní a interakcí
 	{
 		id: 2,
 		type: "Kisk",
@@ -173,6 +174,8 @@ const projects = [
 			}
 		],
 	},
+
+	// ISKM03 Literatura, knihovní procesy a trh
 	{
 		id: 3,
 		type: "Kisk",
@@ -255,6 +258,98 @@ const projects = [
 			// }
 		],
 	},
+
+	// ISKM55 Nástroje a metody datové analytiky
+	{
+	id: 1,
+	type: "Kisk",
+	title: 'ISKM55 Nástroje a metody datové analytiky',
+	category: '1. semestr',
+	img: require('@/assets/images/ISKM55.jpg'),
+	createdDate: '01. 10. 2023',
+	tag: 'kisk',
+	about: 'O předmětu',
+	aboutDetail: [
+	{
+		title: 'Semetr',
+		detail: 'podzim 2023',
+		link: false
+	},
+	{
+		title: 'Ukončení',
+		detail: 'kolokvium',
+		link: false
+	},
+	{
+		title: 'Počet kreditů',
+		detail: '5',
+		link: false
+	},
+	{
+		title: 'Sylabus',
+		detail: 'ISKM55',
+		url: 'https://is.muni.cz/predmet/phil/podzim2023/ISKM55',
+		link: true
+	},
+	{
+		title: 'Soubory',
+		detail: 'OneDrive složka',
+		url: 'https://ucnmuni-my.sharepoint.com/:f:/g/personal/485131_muni_cz/EkmCxEv6eO1Kjq-r9YbyEokB6c8zXJAk7eWf9fwpcD2kAQ?e=ZJxpi5',
+		link: true
+	}
+],
+	descriptionHeading: 'Popis předmětu',
+	descriptionDetail: 'Cílem předmětu je poskytnutí studentům vhled do problematiky datové analýzy a nejpoužívanějších metod či nástrojů pro práci s daty.',
+	toolsHeading: 'Nástroje a dovednosti',
+	toolsDetail: [
+	'CSV',
+	'XML',
+	'API'
+],
+
+	// projectDetailsHeading: 'Předmět informační vědy',
+	// projectDetails: [
+	// {
+	// 	id: 1,
+	// 	details: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla est. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nam quis nulla. Suspendisse nisl. Duis risus. Nulla pulvinar eleifend sem. Maecenas libero. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. '
+	// },
+	//],
+
+	content: [
+	{
+		sectionHeading: 'Úkol 1',
+		sectionId: 1,
+		sectionDate: '2. 10. 2023',
+		sectionParagraphs: [
+			{
+				id: 1,
+				details: 'První úkol se skládá z více částí konkrétně vytvoření CSV souboru zobrazující náš týdenní rozvrh, práce s GPX formátem a vyzkoušení práce s API.'
+			},
+			{
+				id: 2,
+				details: 'CSV soubor jsem navrhla tak, že entitou je jeden vyučovací blok, který obsahuje informace o kódu předmětu, název, místnost v níž výuka probíhá (lokace), den v týdnu a časové údaje od a do. CSV bylo vytvořené v textovém editoru sublime a následně otevřeno v excelu jako CSV soubor. Protože názvy předmmětů obsahují čárky, které jsem původně použila jako oddělovač změnila jsem oddělovač na středník.'
+			},
+			{
+				id: 3,
+				details: 'Pro vizualizaci trasy jsem zvolila cestu z Náměstí Svobody kolem Červeného kostela až k hlavní budově filozofické fakulty. Trasu jsem si naplánovala v mapy.cz odkud lze exportovat gpx soubor. GPX formát je XML schéma, navržené pro data GPS. Pro zobrazení vygenerovaného schématu je použit gpsvisualizer.com.'
+			},
+			{
+				id: 4,
+				details: 'Dalším pod-úkolem bylo zjistit jakým způsobem komunikuje iRIS se serverem, aby v aplikaci mapa.idsjmk.cz vozidla vypadala, že jsou v pohybu. Web pravidělně volá přepisovaný json soubor https://mapa.idsjmk.cz/api/vehicles.json který obsahuje informace o všech aktuálně jedoucích vozidlech. Každé vozidlo má několik atributů - ID, název linky, aktuální polohu GPS (zeměpisná šířka a délka), počíteční a koncovou zastávku a další. Přišlo mi zajímavé, že je použito API map.cz.'
+			},
+			{
+				id: 5,
+				details: 'Vyzkoušela jsem se dotazovat pomocí služby postman na PokeAPI. Musím říct, že jsem si pro zkoušení asi měla vybrat nějaké jednodušší. Vzhledem k množství pokemonů a informací, které o jednotlivých pokémonech jsou nebylo jednoduché se ve výsledku vyznat. Nicméně podařilo se mi získat například seznam všech pokémonů mužského pohlaví (pokeapi.co/api/v2/gender/2/) nebo informace o mém oblíbeném Piplupovi (https://pokeapi.co/api/v2/pokemon/piplup).'
+			},
+			{
+				id: 6,
+				details: 'Snímky obrazovky jsou ve sdílené složce na kterou je odkaz v bočím panelu.'
+			},
+		]
+	}
+],
+},
+
 	// {
 	// 	id: 2,
 	// 	title: 'Phoenix Digital Agency',
